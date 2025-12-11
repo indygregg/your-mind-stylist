66e084c12ff8c06a0302a5fc59b5e07e7f96afff
Admin <wedoventures@gmail.com>
2025-12-11 15:55:52 +0000

    File changes

--- src/pages/PrivateSessions.jsx
+++ src/pages/PrivateSessions.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SEO from "../components/SEO";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "../utils";
 import { motion } from "framer-motion";
