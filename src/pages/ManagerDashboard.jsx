e0cc89d402048af52e337d1ed75dd5167ad23e9a
Admin <wedoventures@gmail.com>
2025-12-11 12:47:36 +0000

    File changes

--- src/pages/ManagerDashboard.jsx
+++ src/pages/ManagerDashboard.jsx
@@ -43,7 +43,6 @@ export default function ManagerDashboard() {
 
   return (
     <div className="min-h-screen bg-[#F9F5EF] py-12 px-6">
-      {user && <OnboardingModal role="manager" />}
       <div className="max-w-7xl mx-auto">
         {/* Welcome Panel */}
         <motion.div
